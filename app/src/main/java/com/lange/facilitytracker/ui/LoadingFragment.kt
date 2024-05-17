364cb616491dd3ed32eea77fd0ea448e6956b2d5
mircoocrim98 <mirco.lange@outlook.com>
2024-05-17 15:27:09 +0200

    create fragments for login,register and loading screen

---
+++ app/src/main/java/com/lange/facilitytracker/ui/LoadingFragment.kt
@@ -0,0 +1,25 @@
+package com.lange.facilitytracker.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+import com.lange.facilitytracker.R
+
+
+class LoadingFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_loading, container, false)
+    }
+}
---
+++ app/src/main/java/com/lange/facilitytracker/ui/LoginFragment.kt
@@ -0,0 +1,30 @@
+package com.lange.facilitytracker.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import com.lange.facilitytracker.MainViewModel
+import com.lange.facilitytracker.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+    private lateinit var binding: FragmentLoginBinding
+    private val viewModel: MainViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
