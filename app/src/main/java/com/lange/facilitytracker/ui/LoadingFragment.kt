5ac7bc193c6e6f6fee699ef715d4c55ce79b4c5d
mircoocrim98 <mirco.lange@outlook.com>
2024-06-07 02:47:54 +0200

    Add initial task resources to the database on app startup
    - Added logic to insert default task resources into the database if none are present

--- app/src/main/java/com/lange/facilitytracker/ui/LoadingFragment.kt
+++ app/src/main/java/com/lange/facilitytracker/ui/LoadingFragment.kt
@@ -9,8 +9,10 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.lange.facilitytracker.MainViewModel
+import com.lange.facilitytracker.TypeOfWorkEnum
 
 import com.lange.facilitytracker.data.model.AuthenticationPayload
+import com.lange.facilitytracker.data.model.TaskResources
 import com.lange.facilitytracker.databinding.FragmentLoadingBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -28,6 +30,7 @@ class LoadingFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentLoadingBinding.inflate(inflater, container, false)
+        viewModel.getAllTasksFromDB()
         return binding.root
     }
 
@@ -37,8 +40,31 @@ class LoadingFragment : Fragment() {
         val sharedPreferences = requireActivity().getSharedPreferences("FTracker", AppCompatActivity.MODE_PRIVATE)
         val sessionToken = sharedPreferences.getString("sessionToken",null)
 
+
+
         CoroutineScope(Dispatchers.Main).launch {
             delay(2000)
+
+            if (viewModel.taskResources.value.isNullOrEmpty()){
+                viewModel.insertAllTasksInDB(
+                    listOf(
+                        TaskResources(text = "Treppenhausreinigung", taskType = 0),
+                        TaskResources(text = "Treppengeländerreinigung", taskType = 0),
+                        TaskResources(text = "Fensterreinigung", taskType = 0),
+                        TaskResources(text = "Speermüllentsorgung", taskType = 0),
+                        TaskResources(text = "Mülltonnenservice", taskType = 0),
+                        TaskResources(text = "Fenster einstellen", taskType = 1),
+                        TaskResources(text = "Durchlauferhitzer austauschen", taskType = 1),
+                        TaskResources(text = "Feuchtigkeitsbeseitigung", taskType = 1),
+                        TaskResources(text = "Streichen", taskType = 1),
+                        TaskResources(text = "Feuchtigkeitsschaden", taskType = 2),
+                        TaskResources(text = "Glasbruch", taskType = 2),
+                        TaskResources(text = "Klingelanlage defekt", taskType = 2),
+                        TaskResources(text = "Graffitis", taskType = 2)
+                    )
+                )
+            }
+
             if (sessionToken == null) {
                 val navController = findNavController()
                 val direction = LoadingFragmentDirections.toLoginFragment()
@@ -61,18 +87,5 @@ class LoadingFragment : Fragment() {
                 }
             }
         }
-
-
-
-
-
-
-//        if (sessionToken != null) {
-//            viewModel.sendToken(sessionToken)
-//        } else {
-//            val navController = findNavController()
-//            val direction = LoadingFragmentDirections.toLoginFragment()
-//            navController.navigate(direction)
-//        }
     }
 }
