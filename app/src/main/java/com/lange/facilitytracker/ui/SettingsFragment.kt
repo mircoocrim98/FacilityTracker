d2693d5c181e1bbb06be8ae4221489657c9302c9
mircoocrim98 <mirco.lange@outlook.com>
2024-06-07 02:38:48 +0200

    added SettingsFragment

---
+++ app/src/main/java/com/lange/facilitytracker/ui/SettingsFragment.kt
@@ -0,0 +1,29 @@
+package com.lange.facilitytracker.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import com.lange.facilitytracker.MainViewModel
+import com.lange.facilitytracker.databinding.FragmentSettingsBinding
+
+class SettingsFragment: Fragment() {
+    private lateinit var binding: FragmentSettingsBinding
+    private val viewModel: MainViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
